c92f338a7d0440082868eb9ef9b83dd33707a772
Alex Korzhev <nfe.unsert@gmail.com>
2020-10-17 13:43:43 +0800

    sqlite_test

--- EnPlus1stTask/Form1.cs
+++ EnPlus1stTask/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SQLite;
 
 namespace EnPlus1stTask
 {
@@ -15,6 +16,38 @@ namespace EnPlus1stTask
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SQLiteConnection.CreateFile("TEST1.sqlite");
+            SQLiteConnection connection;
+            connection = new SQLiteConnection("Data Source=TEST1.sqlite;Version=3;");
+            connection.Open();
+            string sql = "create table highscores (name varchar(20), score int)";
+            SQLiteCommand command = new SQLiteCommand(sql, connection);
+            command.ExecuteNonQuery();
+            sql = "insert into highscores (name, score) values ('Me', 3000)";
+            command = new SQLiteCommand(sql, connection);
+            command.ExecuteNonQuery();
+            sql = "insert into highscores (name, score) values ('Myself', 6000)";
+            command = new SQLiteCommand(sql, connection);
+            command.ExecuteNonQuery();
+            sql = "insert into highscores (name, score) values ('And I', 9001)";
+            command = new SQLiteCommand(sql, connection);
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SQLiteConnection connection = new SQLiteConnection("Data Source=TEST1.sqlite;Version=3;");
+            connection.Open();
+            string sql = "select * from highscores order by score desc";
+            SQLiteCommand command = new SQLiteCommand(sql, connection);
+            SQLiteDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+                Console.WriteLine("Name: " + reader["name"] + "\tScore: " + reader["score"]);
         }
     }
 }
